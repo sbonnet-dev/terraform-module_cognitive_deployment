fd9ad8464a1bb934824a5ef1aa37dace972c279a
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-09 15:51:31 +0200

    completed

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_cognitive_deployment" "gpt-35" {
+  name                 = var.name
+  cognitive_account_id = var.cognitive_account_id//"/subscriptions/01f9de63-11ac-4a41-acd5-e398487749e5/resourceGroups/rg-sboai-env/providers/Microsoft.CognitiveServices/accounts/cog-tvxbgsftoyot5"
+ 
+  model {
+    format  = "OpenAI"
+    name    = "gpt-35-turbo"
+    version = "0301"
+  }
+  scale {
+    type = "Standard"
+  }
+
+  tags = {
+    project     = var.project
+    environment = var.environment
+    owner       = var.owner
+  }
+
+}
