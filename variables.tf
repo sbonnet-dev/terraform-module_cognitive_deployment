6a115fe7841faf35fad9289385366074c0312f50
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-09 15:48:13 +0200

    completed

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  type        = string
+  description = "Resource name"
+}
+
+
+variable "cognitive_account_id" {
+  type        = string
+  description = "Cognitive Account ID"
+}
