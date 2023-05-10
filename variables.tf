3b91d1f841cc41bbfb400dac8d64503eaf52b195
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-10 11:41:32 +0200

    changed nc

--- variables.tf
+++ variables.tf
@@ -8,3 +8,23 @@ variable "cognitive_account_id" {
   type        = string
   description = "Cognitive Account ID"
 }
+
+variable "project" {
+  type        = string
+  description = "Project name"
+  default     = "OSCAR"
+}
+
+variable "environment" {
+  type        = string
+  description = "Specify the type of environement"
+  default     = "dev"
+
+  validation {
+    condition = contains([
+      "dev",
+      "prod",
+    ], var.environment)
+    error_message = "Then environment value is invalid."
+  }
+}
